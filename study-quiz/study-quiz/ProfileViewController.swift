8df06aad875c80a931feadbfced311b75a812c64
David Bielenberg <davidbielenberg@live.de>
2019-11-18 21:14:06 +0100

    adding Kanye

--- study-quiz/study-quiz/ProfileViewController.swift
+++ study-quiz/study-quiz/ProfileViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileHeaderContainer: UIView!
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var iconBox: UIView!
+    @IBOutlet weak var hoursStudiedContainer: UIView!
+    @IBOutlet weak var favCourseContainer: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+        profileHeaderContainer.layer.cornerRadius = 8
+        avatarImage.layer.cornerRadius = avatarImage.frame.height / 2
+        iconBox.layer.cornerRadius = 8
+        hoursStudiedContainer.layer.cornerRadius = 8
+        favCourseContainer.layer.cornerRadius = 8
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

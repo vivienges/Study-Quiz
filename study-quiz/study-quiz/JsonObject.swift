0a99d16004e09bbc6a83ebcbe4a25101c529157e
Vivien Geschwind <vg024@hdm-stuttgart.de>
2019-11-28 17:09:24 +0100

    update JSON

--- study-quiz/study-quiz/JsonObject.swift
+++ study-quiz/study-quiz/JsonObject.swift
@@ -30,26 +30,139 @@ let jsonString =
                 "questions":
                             [
                           {
-                              "title": "wie gehts?",
+                              "title": "Question1?",
                               "answeredRight": false
                           },
                         {
-                            "title": "wie gehts?",
+                            "title": "Question2?",
                             "answeredRight": false
                         },
                         {
-                            "title": "wie gehts?",
+                            "title": "Question3?",
                             "answeredRight": false
                         }
                         ],
                       
                 "answers":[
-                      ["Answer1", "Answer1", "Answer1", "Answer1"]
+                      ["Answer1", "Answer1", "Answer1", "Answer1"],
+                    ["Answer2", "Answer2", "Answer2", "Answer2"],
+["Answer3", "Answer3", "Answer3", "Answer3"]
+
                       ]
                 
                 }
             ]
-       }
+       },
+{
+      "isbn":"1234",
+      "title": "df",
+      "publisher": "efrg",
+      "releaseYear": "efgr",
+      "coverImage": "efwgr",
+      "summary": "ewgr",
+      "description": "wgr",
+     "quiz": [
+             {
+             "questions":
+                         [
+                       {
+                           "title": "Question1?",
+                           "answeredRight": false
+                       },
+                     {
+                         "title": "Question2?",
+                         "answeredRight": false
+                     },
+                     {
+                         "title": "Question3?",
+                         "answeredRight": false
+                     }
+                     ],
+                   
+             "answers":[
+                   ["Answer1", "Answer1", "Answer1", "Answer1"]
+                   ]
+             
+             }
+         ]
+    }
+   ]
+},
+{
+   "title":"Second course",
+   "teacher":"Mikael Bergman",
+   "description":"Käsekuchen",
+   "totalQuestions":20,
+   "books":[
+      {
+         "isbn":"1234",
+         "title": "df",
+         "publisher": "efrg",
+         "releaseYear": "efgr",
+         "coverImage": "efwgr",
+         "summary": "ewgr",
+         "description": "wgr",
+        "quiz": [
+                {
+                "questions":
+                            [
+                          {
+                              "title": "Question1?",
+                              "answeredRight": false
+                          },
+                        {
+                            "title": "Question2?",
+                            "answeredRight": false
+                        },
+                        {
+                            "title": "Question3?",
+                            "answeredRight": false
+                        }
+                        ],
+                      
+                "answers":[
+                      ["Answer1", "Answer1", "Answer1", "Answer1"],
+                    ["Answer2", "Answer2", "Answer2", "Answer2"],
+["Answer3", "Answer3", "Answer3", "Answer3"]
+
+                      ]
+                
+                }
+            ]
+       },
+{
+      "isbn":"1234",
+      "title": "df",
+      "publisher": "efrg",
+      "releaseYear": "efgr",
+      "coverImage": "efwgr",
+      "summary": "ewgr",
+      "description": "wgr",
+     "quiz": [
+             {
+             "questions":
+                         [
+                       {
+                           "title": "Question1?",
+                           "answeredRight": false
+                       },
+                     {
+                         "title": "Question2?",
+                         "answeredRight": false
+                     },
+                     {
+                         "title": "Question3?",
+                         "answeredRight": false
+                     }
+                     ],
+                   
+             "answers":[
+                   ["Answer1", "Answer1", "Answer1", "Answer1"]
+                   ]
+             
+             }
+         ]
+    }
    ]
 }
 ]
